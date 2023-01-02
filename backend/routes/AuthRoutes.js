8658b5d8101c69bd0c6a91e2025aac484c6b2782
gautampraveen <gautampraveen351@gmail.com>
2023-01-02 12:35:55 +0530

    few

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,25 +1,27 @@
 const {
-  signupfunc,
-  deletAll,
-  loginuser,
-  logout,
-  isAuth,
-  getDetails,
-  deletMe,
-  forgetPassword,
-  resetPassword,
-  addEmail,
-} = require("../controllers/authController");
+	signupfunc,
+	deletAll,
+	loginuser,
+	logout,
+	isAuth,
+	getDetails,
+	deletMe,
+	forgetPassword,
+	resetPassword,
+	addEmail
+} = require('../controllers/authController');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
 // sign up route
-router.route("/register").post(signupfunc).delete(isAuth, deletAll);
-router.route("/login").post(loginuser);
-router.route("/logout").get(isAuth, logout);
-router.route("/me").get(isAuth, getDetails).delete(isAuth, deletMe);
-router.route("/addchat").post(isAuth, addEmail);
-
-router.route("/forget").post(forgetPassword);
-router.route("/resetPassword").post(resetPassword);
+router.route('/register').post(signupfunc).delete(isAuth, deletAll);
+router.route('/login').post(loginuser);
+router.route('/logout').get(isAuth, logout);
+router.route('/me').get(isAuth, getDetails).delete(isAuth, deletMe);
+router.route('/addchat').post(isAuth, addEmail);
+router.get('/', async (req, res, next) => {
+	alert('running fine');
+});
+router.route('/forget').post(forgetPassword);
+router.route('/resetPassword').post(resetPassword);
 module.exports = router;
